e659d991443f1d5aac61609331f9dd920a331e90
nguyenvinhdat642 <130133531+nguyenvinhdat642@users.noreply.github.com>
2024-03-13 18:11:13 +0700

    .

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ app.get('/', indexController.getIndex);
 
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+    console.log(`Server is running on port http://localhost:${process.env.PORT}/`);
 }
 );
